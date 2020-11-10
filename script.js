fae7935e45a508fa5f6d0ba0f47f05d871576136
Filip M <filip.misilo@gmail.com>
2020-11-10 22:27:35 +0100

    initial push

---
+++ script.js
@@ -0,0 +1,6 @@
+const difficulty = {
+    BEGINNER: {mines: 10, rows: 9, columns: 9},
+    INTERMEDIATE: {mines: 40, rows: 15, columns: 15},
+    EXPERT: {mines: 99, rows: 30, columns: 16},
+    CUSTOM: {mines: 200, rows: 35, columns: 35}
+}
