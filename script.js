6f6b0b34bd048434b231910e402deedf64765950
Filip M <filip.misilo@gmail.com>
2020-11-29 19:22:12 +0100

    Meaningful commit name

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
 //TODO:
-//remove hover
 //change eventListener for rightClick
 //gray background with results after win
 //boom emojis
 //fix firefox toElement (target)
 //rewrite scss
 //rewrite with jQuery
-//rewrite with angular(js)
 //debug mode
 //refactor elements to variables
 //update html
+//change emoji on win
+//tests
 //custom mode
-//add button styling to emoji
-//update remainingCells to remainingMines and decrement it via flags
-//WINGAME zle sie liczy i nie wygrywa przy minach === 0
+//animacje jak w MS minesweepera
+//replace loops with methods
+//sharing links
+//gra nie stopuje po odkryciu wszystkich pol na beginnerze po f5 - nie zawsze
+//context menu odpala sie na odkrytych polach
+// https://airbnb.io/javascript/
 
 const DIFFICULTY = {
     beginner: {mines: 10, rows: 10, columns: 10},
     intermediate: {mines: 40, rows: 15, columns: 15},
-    expert: {mines: 99, rows: 30, columns: 16},
+    expert: {mines: 99, rows: 16, columns: 30},
     custom: {}
 }
+
+const MINEFIELD_TABLE = document.querySelector(".mineTable");
+const DIFFICULTY_SELECTOR = document.querySelector("select");
+const RESTART_BUTTON = document.getElementById("emojiRestart");
+const TIME_COUNTER = document.getElementById("timeCounter");
+const REMAINING_MINES_COUNTER = document.getElementById("remainingMinesCounter");
+const APP_BODY = document.querySelector("body");
+const MINEFIELD = [];
+
+let selectedDifficulty = DIFFICULTY.beginner;
+let remainingMines = selectedDifficulty.mines;
+let remainingCoveredCells = selectedDifficulty.columns * selectedDifficulty.rows - selectedDifficulty.mines;
+let isGameOn = false;
+let timer = 0;
+let timerIntervalID;
 
 class Cell {
     isUncovered = false;
@@ -30,42 +48,71 @@ class Cell {
     displayText = "";
 }
 
+prepareGame();
 
-const MINEFIELD = [];
-let selectedDifficulty = DIFFICULTY.beginner;
-let remainingMines = selectedDifficulty.mines;
-let remainingUncoveredCells = selectedDifficulty.columns * selectedDifficulty.rows - selectedDifficulty.mines;
-let isGameOn = false;
-let timer = 0;
-let timerIntervalID;
-
-const MINEFIELD_TABLE = document.querySelector(".mineTable");
-const DIFFICULTY_SELECTOR = document.querySelector("select");
-const RESTART_BUTTON = document.getElementById("emojiRestart");
-const TIME_COUNTER = document.getElementById("timeCounter");
-const REMAINING_MINES_COUNTER = document.getElementById("remainingMinesCounter");
-
-initGame();
-
-function initGame() {
+function prepareGame() {
     switchEmojiToCool();
-    initElements();
-    initMinefield();
-    initMines();
-    createMinefieldHTML();
+    createEventListeners();
+    createMinefield();
+    populateMinefield();
+    updateCellDisplayText();
+    renderMinefield();
 }
 
-function initElements() {
+function createEventListeners() {
+    DIFFICULTY_SELECTOR.onchange = switchDifficulty;
+    REMAINING_MINES_COUNTER.innerText = String(remainingMines);
     RESTART_BUTTON.addEventListener("click", restartGame);
     MINEFIELD_TABLE.addEventListener("click", leftClick);
     MINEFIELD_TABLE.addEventListener("contextmenu", rightClick);
     MINEFIELD_TABLE.addEventListener("mousedown", switchEmojiToScared);
-    DIFFICULTY_SELECTOR.onchange = restartGame;
-    document.querySelector("body").addEventListener("mouseup", switchEmojiToCool);
-    REMAINING_MINES_COUNTER.innerText = String(remainingMines);
+    APP_BODY.addEventListener("mouseup", switchEmojiToCool);
 }
 
-function initMinefield() {
+function preventClickingAfterGameEnds() {
+    MINEFIELD_TABLE.removeEventListener("click", leftClick);
+    MINEFIELD_TABLE.removeEventListener("contextmenu", rightClick);
+    MINEFIELD_TABLE.addEventListener("contextmenu", (event) => event.preventDefault());
+    MINEFIELD_TABLE.removeEventListener("mousedown", switchEmojiToScared);
+    APP_BODY.removeEventListener("mouseup", switchEmojiToCool);
+}
+
+
+
+function gameOver() {
+    stopTimer(timerIntervalID);
+    preventClickingAfterGameEnds();
+    console.log("GAME OVER!");
+    RESTART_BUTTON.innerText = '☠️';
+    //TODO: dodac wybuchy idace naokolo
+}
+
+function winGame() {
+    stopTimer(timerIntervalID);
+    RESTART_BUTTON.innerText = '🏆';
+    preventClickingAfterGameEnds();
+    console.log("YOU WIN!");
+}
+
+function restartGame() {
+    stopTimer();
+    resetTimer();
+
+    isGameOn = false;
+    MINEFIELD_TABLE.innerHTML = "";
+    remainingMines = selectedDifficulty.mines;
+    REMAINING_MINES_COUNTER.innerText = String(remainingMines);
+    remainingCoveredCells = selectedDifficulty.columns * selectedDifficulty.rows - selectedDifficulty.mines;
+
+    wipeMines();
+    preventClickingAfterGameEnds();
+    createEventListeners();
+    prepareGame();
+}
+
+// ########################################################################################################
+
+function createMinefield() {
     for (let i = 0; i < selectedDifficulty.rows; i++) {
         MINEFIELD.push([]);
         for (let j = 0; j < selectedDifficulty.columns; j++) {
@@ -74,27 +121,19 @@ function initMinefield() {
     }
 }
 
-function wipeMines() {
-    for (let i = 0; i < selectedDifficulty.columns; i++) {
-        for (let j = 0; j < selectedDifficulty.rows; j++) {
-            MINEFIELD[i][j] = new Cell();
-        }
-    }
-}
+function populateMinefield() {
+    let minesLeftToDistribute = selectedDifficulty.mines;
 
-function initMines() {
-    let minesToDistribute = selectedDifficulty.mines;
-    let row, column;
+    while (minesLeftToDistribute) {
+        const row = getRandomValue(selectedDifficulty.rows);
+        const column = getRandomValue(selectedDifficulty.columns);
+        const cell = MINEFIELD[row][column];
 
-    while (minesToDistribute) {
-        row = getRandomValue(selectedDifficulty.rows);
-        column = getRandomValue(selectedDifficulty.columns);
-        if (!MINEFIELD[row][column].isMine) {
-            MINEFIELD[row][column].value = '-1';
-            MINEFIELD[row][column].isMine = true;
-            MINEFIELD[row][column].displayText = '💣';
+        if (!cell.isMine) {
+            cell.value = '-1';
+            cell.isMine = true;
             incrementAdjacentCells(row, column);
-            minesToDistribute--;
+            minesLeftToDistribute--;
         }
     }
 }
@@ -109,174 +148,74 @@ function incrementAdjacentCells(row, column) {
     }
 }
 
-function createMinefieldHTML() {
-    let newElement;
-
+function renderMinefield() {
     for (let i = 0; i < selectedDifficulty.rows; i++) {
-        newElement = document.createElement("tr");
+        let newElement = document.createElement("TR");
+
         newElement.classList.add(`row-${i}`);
         MINEFIELD_TABLE.appendChild(newElement);
+
+        for (let j = 0; j < selectedDifficulty.columns; j++) {
+            newElement = document.createElement("TD");
+            newElement.id = `cell-${i}-${j}`;
+            document.querySelector(`.row-${i}`).appendChild(newElement);
+        }
+    }
+}
+
+function updateCellDisplayText() {
+    for (let i = 0; i < selectedDifficulty.rows; i++) {
         for (let j = 0; j < selectedDifficulty.columns; j++) {
             let cell = MINEFIELD[i][j];
 
-            newElement = document.createElement("td");
-            newElement.id = `cell-${i}-${j}`;
-            document.querySelector(`.row-${i}`).appendChild(newElement);
-
             if (cell.value === 0) {
                 cell.displayText = ' ';
-            }
-
-            if (cell.value > 0) {
+            } else if (cell.value > 0) {
                 cell.displayText = cell.value;
-            }
-        }
-    }
-}
-
-function uncoverSingleCell(row, column) {
-    let uncoveredNode = document.createTextNode(MINEFIELD[row][column].displayText);
-    let cell = document.getElementById(`cell-${row}-${column}`);
-
-    cell.appendChild(uncoveredNode);
-    cell.classList.add("uncovered");
-    cell.classList.add(`cell-${MINEFIELD[row][column].value}`);
-    MINEFIELD[row][column].isUncovered = true;
-    remainingUncoveredCells--;
-
-    if (MINEFIELD[row][column].value === 0) {
-        uncoverAdjacentCells(row, column);
-    }
-}
-
-function uncoverAdjacentCells(row, column) {
-    for (let i = row - 1; i <= row + 1; i++) {
-        for (let j = column - 1; j <= column + 1; j++) {
-            if (isWithinBounds(i, j) && !MINEFIELD[i][j].isUncovered) {
-                uncoverSingleCell(i, j);
-            }
-        }
-    }
-}
-
-function leftClick(event) {
-    let row, column;
-
-    if (event.target.tagName === "TD") {
-        let coordinates = event.toElement.id.split("-");
-        row = Number(coordinates[1]);
-        column = Number(coordinates[2]);
-    } else {
-        return;
-    }
-
-    let cell = MINEFIELD[row][column];
-
-    if (!cell.isUncovered && !cell.isFlagged) {
-        if (cell.isMine) {
-            gameOver();
-        } else {
-            if (cell.value > 0 && remainingUncoveredCells === 1) {
-                winGame();
             } else {
-                startTimer();
+                cell.displayText = '💣';
             }
         }
-        uncoverSingleCell(row, column);
     }
 }
 
-function rightClick(event) {
-    let row, column;
-
-    if (event.target.tagName === "TD") {
-        let coordinates = event.toElement.id.split("-");
-        row = coordinates[1];
-        column = coordinates[2];
-        event.preventDefault();
-    } else {
-        return;
+function wipeMines() {
+    for (let i = 0; i < selectedDifficulty.rows; i++) {
+        MINEFIELD.pop();
     }
+}
 
-    let cell = MINEFIELD[row][column];
-    if (!cell.isUncovered) {
-        let element = document.getElementById(`cell-${row}-${column}`);
+function switchDifficulty() {
+    selectedDifficulty = DIFFICULTY[DIFFICULTY_SELECTOR.value];
+    restartGame();
+}
 
-        if (element.innerText === '🚩') {
-            element.innerText = ' ';
-            cell.isFlagged = false;
-            REMAINING_MINES_COUNTER.innerText = String(Number(REMAINING_MINES_COUNTER.innerText) + 1);
-        } else {
-            element.innerText = '🚩';
-            cell.isFlagged = true;
-            REMAINING_MINES_COUNTER.innerText = String(REMAINING_MINES_COUNTER.innerText - 1);
-        }
-    }
+function startGame() {
+    isGameOn = true;
+    startTimer();
 }
 
 function startTimer() {
-    if (!isGameOn) {
-        isGameOn = true;
-        timerIntervalID = setInterval(() => {
-            timer++;
-            TIME_COUNTER.innerText = String(timer);
-        }, 1000);
-    }
+    timerIntervalID = setInterval(() => {
+        TIME_COUNTER.innerText = String(++timer);
+    }, 1000);
 }
 
-function gameOver() {
-    clearInterval(timerIntervalID);
-    preventClicking();
-    console.log("GAME OVER!");
-    RESTART_BUTTON.innerText = '☠️';
-}
-
-function winGame() {
-    clearInterval(timerIntervalID);
-    preventClicking();
-    console.log("YOU WIN!");
-}
-
-function restartGame(event) {
-    let target = event.target.outerHTML;
-
-    if (target.includes("emoji")) {
-        console.log("Resetting via button");
-    } else {
-        console.log("Resetting via select");
-        let selectValue  = DIFFICULTY_SELECTOR.value.toUpperCase();
-        selectedDifficulty = DIFFICULTY[selectValue];
-    }
-    MINEFIELD_TABLE.innerHTML = "";
-
-    clearInterval(timerIntervalID);
-
-    timer = 0;
-    isGameOn = false;
+function resetTimer() {
     TIME_COUNTER.innerText = '0';
-    remainingMines = selectedDifficulty.mines;
-    REMAINING_MINES_COUNTER.innerText = String(remainingMines);
-    wipeMines();
-    initElements();
-    initGame();
+    timer = 0;
 }
 
-function preventClicking() {
-    MINEFIELD_TABLE.removeEventListener("click", leftClick);
-    MINEFIELD_TABLE.removeEventListener("contextmenu", rightClick);
-    MINEFIELD_TABLE.removeEventListener("mousedown", switchEmojiToScared);
-    document.querySelector("body").removeEventListener("mouseup", switchEmojiToCool);
+function stopTimer() {
+    clearInterval(timerIntervalID);
 }
-
-// ########################################################################################################
-
 
 function isWithinBounds(row, column) {
     return (
-        row >= 0 &&
-        row < selectedDifficulty.rows &&
-        column >= 0 &&
-        column < selectedDifficulty.columns
+        row >= 0
+        && row < selectedDifficulty.rows
+        && column >= 0
+        && column < selectedDifficulty.columns
     )
 }
 
@@ -292,5 +231,79 @@ function switchEmojiToScared(event) {
     if (event.button === 0) {
         RESTART_BUTTON.innerText = '😮';
         event.preventDefault();
+    }
+}
+
+function leftClick(event) {
+    if (event.target.tagName !== "TD") {
+        return;
+    }
+    const coordinates = event.toElement.id.split("-");
+    const row = Number(coordinates[1]);
+    const column = Number(coordinates[2]);
+    const cell = MINEFIELD[row][column];
+
+    //todo: remove log
+    console.log(remainingCoveredCells);
+    if (!cell.isUncovered && !cell.isFlagged) {
+        if (cell.isMine) {
+            gameOver();
+        } else {
+            if (cell.value > 0 && remainingCoveredCells === 1) {
+                winGame();
+            } else if (!isGameOn) {
+                startGame();
+            }
+        }
+        uncoverSingleCell(row, column);
+    }
+}
+
+function rightClick(event) {
+    const coordinates = event.toElement.id.split("-");
+    const row = coordinates[1];
+    const column = coordinates[2];
+    const cell = MINEFIELD[row][column];
+
+    if (!cell.isUncovered) {
+        const element = document.getElementById(`cell-${row}-${column}`);
+
+        event.preventDefault();
+
+        if (element.innerText === '🚩') {
+            element.innerText = ' ';
+            cell.isFlagged = false;
+            REMAINING_MINES_COUNTER.innerText = String(Number(REMAINING_MINES_COUNTER.innerText) + 1);
+        } else {
+            element.innerText = '🚩';
+            cell.isFlagged = true;
+            REMAINING_MINES_COUNTER.innerText = String(REMAINING_MINES_COUNTER.innerText - 1);
+        }
+    }
+}
+
+function uncoverSingleCell(row, column) {
+    const uncoveredNode = document.createTextNode(MINEFIELD[row][column].displayText);
+    const cell = document.getElementById(`cell-${row}-${column}`);
+
+    cell.appendChild(uncoveredNode);
+    cell.classList.add("uncovered");
+    cell.classList.add(`cell-${MINEFIELD[row][column].value}`);
+
+    MINEFIELD[row][column].isUncovered = true;
+    remainingCoveredCells--;
+
+    if (MINEFIELD[row][column].value === 0) {
+        uncoverAdjacentCells(row, column);
+    }
+}
+
+function uncoverAdjacentCells(row, column) {
+    for (let i = row - 1; i <= row + 1; i++) {
+        for (let j = column - 1; j <= column + 1; j++) {
+            if (isWithinBounds(i, j) && !MINEFIELD[i][j].isUncovered && !MINEFIELD[i][j].isFlagged) {
+                uncoverSingleCell(i, j);
+            }
+        }
     }
 }
